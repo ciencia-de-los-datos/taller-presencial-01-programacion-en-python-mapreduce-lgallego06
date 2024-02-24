dda9f1d37a4f7b27a5a744ef76e12e03a1ecca21
Lili Gallego <lgallego@unal.edu.co>
2024-02-23 20:40:16 -0500

    Intento 1
    Taller 1

--- word_count.py
+++ word_count.py
@@ -16,7 +16,20 @@
 def load_input(input_directory):
     pass
 
+import glob 
+import fileinput
+def load_input(input_directory):
+    dir_path=input_directory +'/*.txt'
+    sequence=[]
+    filenames=glob.glob(dir_path)
+    for filename in filenames:
+        with fileinput.input(files=filenames) as file:
+            for line in file:
+                tupla=(fileinput.filename(), line)
+                sequence.append(tupla)
+        return sequence
 
+#load_input('input')
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
 # función anterior y retorna una lista de tuplas (clave, valor). En este caso,
@@ -30,7 +43,19 @@ def load_input(input_directory):
 #   ]
 #
 def mapper(sequence):
-    pass
+    new_sequence = []
+    for _, text in sequence:
+        words = text.split()
+        for word in words:
+            word = word.replace(",", "")
+            word = word.replace(".", "")
+            word=word.lower()
+            new_sequence.append((word,1))
+    return new_sequence
+
+#sequence=load_input('input')
+#sequence=mapper(sequence)
+#print(sequence)
 
 
 #
@@ -45,7 +70,14 @@ def mapper(sequence):
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sorted_sequence=sorted(sequence, key=lambda x:x[0])
+    return sorted_sequence
+#sequence=load_input('input')
+#sequence= mapper(sequence)
+#sequence= shuffle_and_sort(sequence)
+#print(sequence)
+
+    
 
 
 #
@@ -55,15 +87,41 @@ def shuffle_and_sort(sequence):
 # texto.
 #
 def reducer(sequence):
-    pass
+    diccionario={}
+    for key, value in sequence:
+        if key not in diccionario.keys():
+            diccionario[key]=[]
+        diccionario[key].append(value)
+    new_sequence=[]
+    for key, value in diccionario.items():
+        tupla=(key, sum(value))
+        new_sequence.append(tupla)
+    return new_sequence
+
+#sequence=load_input('input')
+#sequence=mapper(sequence)
+#sequence=shuffle_and_sort(sequence)
+#reducer(sequence)
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
-def create_ouptput_directory(output_directory):
-    pass
+import os
+
+def create_output_directory(output_directory):
+    if os.path.exists(output_directory):
+        raise FileExistsError(f"The directory '{output_directory}' already exists.")
+    os.makedirs(output_directory)
+    return output_directory  # Devolver el nombre del directorio creado
+
+#sequence=load_input('input')
+#sequence=mapper(sequence)
+#sequence=shuffle_and_sort(sequence)
+#sequence=reducer(sequence)
+#create_output_directory('output')
+
 
 
 #
@@ -75,7 +133,16 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    with open(output_directory +'/part-0000','w') as file:
+        for key, value in sequence:
+            file.write(f"{key}\t{value}\n")
+
+#sequence = load_input('input')
+#sequence = mapper(sequence)
+#sequence = shuffle_and_sort(sequence)
+#sequence = reducer(sequence)
+#output_directory = create_output_directory('output')  # Capturar el nombre del directorio creado
+#save_output(output_directory, sequence)
 
 
 #
@@ -83,14 +150,22 @@ def save_output(output_directory, sequence):
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open(output_directory + '\_SUCCESS','w') as file:
+        file.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    output_directory = create_output_directory(output_directory)  # Capturar el nombre del directorio creado
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+
 
 
 if __name__ == "__main__":
